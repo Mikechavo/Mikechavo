0dac6d768eb704aa82a954e01d8d6f7f01ce8599
mikechavo <mchavez76@yahoo.com>
2023-08-30 20:03:33 -0700

    icon fix

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -7,20 +7,22 @@ import emailLogo from '../icons/emailLogo.png';
 const ContactMe = () => {
   return (
   <section id="Contact">
+    <p>Contact Me:</p>
     
-    <ul>
-      <p>
+      
         <div className="email-icon">
           <a href="mailto:mchavez76@yahoo.com">
             <img src={emailLogo} alt="Email" className="icon-logo" />
           </a>
-        </div>
-      </p>
+       
+      
       <a href="https://github.com/Mikechavo"><img src={github} alt="GitHub" className="icon-logo" /></a>
-      <p>
+      
         <a href="https://www.linkedin.com/in/michael-t-chavez/"><img src={linkedin} alt="LinkedIn" className="icon-logo" /></a>
-      </p>
-    </ul>
+      
+      </div>
+   
+    
   </section>
   )
 }
