ca8f6f2ec3eee75799ee31968ea0d784f2f3c0f8
Mikechavo <mchavez76@yahoo.com>
2024-01-31 18:36:38 -0600

    working on sizing for resume tab

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,30 +2,50 @@
 import React from 'react';
 import { Document, Page } from 'react-pdf';
 import { Card, Button, Input, Label, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
-import '../App.css';
 
 const Resume = () => {
   const resumePath = process.env.PUBLIC_URL + '/resume/MichaelChavez_Resume_Jan_2024.pdf';
-
   return (
     <div className='cards'>
-      <Card color="" style={{ width: '40rem' }}>
-        <h2>My Resume</h2>
-        <Document file={resumePath}>
-          {/* Customize the style of the Page component */}
-          <Page 
-            pageNumber={1}
-            style={{
-              display: 'block',
-              userSelect: 'none',
-              width: '100%', // Set the width to 100% for full width
-              height: 'auto', // Automatically adjust the height
-            }}
-          />
-        </Document>
-      </Card>
+      <Document file={resumePath}>
+        <Page pageNumber={1} />
+      </Document>
+      
     </div>
+    
   );
 };
 
 export default Resume;
+
+
+// // Import necessary components and styles
+// import React from 'react';
+// import { Document, Page } from 'react-pdf';
+// import { Card, Button, Input, Label, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
+
+// const Resume = () => {
+//   const resumePath = process.env.PUBLIC_URL + '/resume/MichaelChavez_Resume_Jan_2024.pdf';
+
+//   return (
+//     <div className='cards'>
+//       <Card color="" style={{ width: '40rem' }}>
+//         <h2>My Resume</h2>
+//         <Document file={resumePath}>
+//           {/* Customize the style of the Page component */}
+//           <Page 
+//             pageNumber={1}
+//             style={{
+//               display: 'block',
+//               userSelect: 'none',
+//               width: '100%', // Set the width to 100% for full width
+//               height: 'auto', // Automatically adjust the height
+//             }}
+//           />
+//         </Document>
+//       </Card>
+//     </div>
+//   );
+// };
+
+// export default Resume;
